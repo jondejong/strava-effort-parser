350ce7d29f14f285f1cbd3fe05f24a1872ec3a3e
Jon DeJong <jon.dejong@gmail.com>
2023-10-18 16:54:21 -0400

    Adding weekly distance totals for biking and running

---
+++ app/src/main/kotlin/strava/effort/parser/mapper/activity/biking/BikingTotalWeekMapper.kt
@@ -0,0 +1,6 @@
+package strava.effort.parser.mapper.activity.biking
+
+import strava.effort.parser.ActivityType
+import strava.effort.parser.mapper.activity.WeekAggregator
+
+class BikingTotalWeekMapper: WeekAggregator(ActivityType.BIKE)
